ccb59d1a93f21292dd5fcae6387a1ee499a4f82f
Nils154 <NilsGokemeijer@yahoo.com>
2022-10-09 18:31:47 -0500

    fixed depreciated function use

--- custom_components/hdhomerun/config_flow.py
+++ custom_components/hdhomerun/config_flow.py
@@ -2,7 +2,14 @@
 from homeassistant.helpers import config_entry_flow
 from homeassistant import config_entries
 
-from hdhr.adapter import HdhrUtility
+try:
+    from hdhr.adapter import HdhrUtility
+except ModuleNotFoundError:
+    _LOGGER.exception(
+        '''libhdhomerun missing: install with:
+            sudo docker exec -it homeassistant bash
+            apk add libhdhomerun'''
+    )
 
 from .const import DOMAIN
 
@@ -12,9 +19,11 @@ async def _async_has_devices(hass):
     hdhr_devices = HdhrUtility.discover_find_devices_custom()
     return len(hdhr_devices) > 0
 
-config_entry_flow.register_discovery_flow(
-    DOMAIN,
-    'HDHomeRun',
-    _async_has_devices,
-    config_entries.CONN_CLASS_LOCAL_POLL
-)
+
+@config_entries.HANDLERS.register(DOMAIN)
+class HDHomeRunConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Example config flow."""
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle user step."""
